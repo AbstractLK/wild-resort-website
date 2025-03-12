b16a4ae961e46ac9300bc6dec351ab5e9c0a66c6
Kaveesha <kalshandhanasekara@gmail.com>
2025-03-12 21:23:48 +0530

    add side nevigation panel

---
+++ app/_components/SideNavigation.js
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import {
+  CalendarDaysIcon,
+  HomeIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
+import SignOutButton from "./SignOutButton";
+
+const navLinks = [
+  {
+    name: "Home",
+    href: "/account",
+    icon: <HomeIcon className="h-5 w-5 text-slate-400" />,
+  },
+  {
+    name: "Reservations",
+    href: "/account/reservations",
+    icon: <CalendarDaysIcon className="h-5 w-5 text-slate-400" />,
+  },
+  {
+    name: "Guest profile",
+    href: "/account/profile",
+    icon: <UserIcon className="h-5 w-5 text-slate-400" />,
+  },
+];
+
+function SideNavigation() {
+  return (
+    <nav className="border-r border-slate-700">
+      <ul className="flex flex-col gap-2 h-full text-lg">
+        {navLinks.map((link) => (
+          <li key={link.name}>
+            <Link
+              className={`py-3 px-5 text-slate-400 hover:bg-slate-800 hover:text-slate-200 transition-colors flex items-center gap-4 font-semibold `}
+              href={link.href}
+            >
+              {link.icon}
+              <span>{link.name}</span>
+            </Link>
+          </li>
+        ))}
+
+        <li className="mt-auto">
+          <SignOutButton />
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default SideNavigation;
