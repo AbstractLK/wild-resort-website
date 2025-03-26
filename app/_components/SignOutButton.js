765e9e7917e27cf50f809c69333db1d4e1774563
Kaveesha <kalshandhanasekara@gmail.com>
2025-03-26 22:08:56 +0530

    add signOut function

--- app/_components/SignOutButton.js
+++ app/_components/SignOutButton.js
@@ -1,11 +1,14 @@
 import { ArrowRightStartOnRectangleIcon } from '@heroicons/react/24/solid';
+import { signOutAction } from '../_lib/actions';
 
 function SignOutButton() {
   return (
-    <button className='w-full py-3 px-5 text-slate-400 hover:bg-slate-800 hover:text-slate-200 transition-colors gap-4 font-semibold flex items-center'>
+    <form action={signOutAction}>
+      <button className='w-full py-3 px-5 text-slate-400 hover:bg-slate-800 hover:text-slate-200 transition-colors gap-4 font-semibold flex items-center'>
       <ArrowRightStartOnRectangleIcon className="h-5 w-5 text-slate-400" />
       <span>Sign out</span>
     </button>
+    </form>
   );
 }
 
