f9a6041b9b0d10ccf6fbcda40684503cf8a68a83
Kaveesha <kalshandhanasekara@gmail.com>
2025-08-14 00:43:32 +0530

    test

--- app/login/page.js
+++ app/login/page.js
@@ -9,7 +9,7 @@ export default function page() {
   return (
     <div className="flex flex-col gap-6 sm:gap-10 mt-6 sm:mt-10 items-center px-4 sm:px-6 text-center max-w-md mx-auto">
       <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold">
-        Sign in to access your guest area!
+        Sign in to access your guest area!!
       </h2>
       <SignInButton className="w-full sm:w-auto" />
     </div>
