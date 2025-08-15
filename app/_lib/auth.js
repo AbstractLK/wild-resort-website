572116e5d26d507af1d0929298194db6b7558ecf
Kaveesha <kalshandhanasekara@gmail.com>
2025-08-15 14:28:33 +0530

    Revert new

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,4 +1,38 @@
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
+import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
+
+const authConfig = {
+    providers: [
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET
+        })
+    ],
+    callbacks: {
+        authorized({auth, request}) {
+            return !!auth?.user;
+        },
+        async signIn({user}) {
+            try {
+                const existingGuest = await getGuest(user.email);
+                if (!existingGuest){
+                    await createGuest({fullName: user.name, email: user.email});
+                }
+                return true;
+            } catch {
+                return false;
+            }
+        },
+        async session({session}){
+            const guest = await getGuest(session.user.email);
+            session.user.guestId = guest.id;
+            return session;
+        }
+    },
+    pages: {
+        signIn: "/login",
+    }
+};
 
 export const {auth, signIn, signOut, handlers: {GET, POST}} = NextAuth(authConfig);
